79a4976f513f5e791bfd38beec8fc33b163d37f6
tienanhjack <145272478+tienanhjack@users.noreply.github.com>
2024-01-03 22:13:11 +0700

    Add files via upload

---
+++ newyeaer.py
@@ -0,0 +1,9 @@
+from calendar import*
+year = int(input('Enter Year:'))
+print(calendar(year, 2, 2, 8, 3))
+
+# 2=2 character for days (Mo, tu, etc)
+# 1=1 line (row) for each week
+# 8=8 rows for each month
+# 3=3 columns for all months of the year.
+# clcoding.com
